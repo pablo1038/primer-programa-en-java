e940f0472565d80aee352f0ea913cd3f5a44f98b
Pablo1038 <pablogutierrezramos1038rgmail.com>
2020-10-14 09:57:16 +0200

    Se le añade otra linea adicional con la localidad de mi residencia actual y mi sistema operativo

--- prueba.java
+++ prueba.java
@@ -4,6 +4,7 @@ public class prueba{
     
         System.out.println("Pablo Gutiérrez Ramos");
         System.out.println("Nací en León en 2002");
+        System.oiut.println("Actualmente vivo en Eras de Renueva en León y tengo instalado windows 10");
         
     }
 }
