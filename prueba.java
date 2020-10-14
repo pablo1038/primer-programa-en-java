6ac55f096e3784aeb134ef0d9e0567cc611d5b23
Pablo1038 <pablogutierrezramos1038rgmail.com>
2020-10-14 09:52:45 +0200

    Se le añade una linea adicional con la localidad y el año de nacimiento

--- prueba.java
+++ prueba.java
@@ -3,6 +3,7 @@ public class prueba{
     public static void main (String args[]) {
     
         System.out.println("Pablo Gutiérrez Ramos");
+        System.out.println("Nací en León en 2002");
         
     }
 }
