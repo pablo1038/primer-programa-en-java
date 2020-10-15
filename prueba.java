8dd8812310e11b9e8fc58c42bcf69078073a0cde
Pablo1038 <pablogutierrezramos1038rgmail.com>
2020-10-15 09:36:00 +0200

    Esta vez añadimos una nueva linea con mi nombre compelto

--- prueba.java
+++ prueba.java
@@ -2,9 +2,11 @@ public class prueba{
 
     public static void main (String args[]) {
     
-        System.out.println("Pablo Gutiérrez Ramos");
+        System.out.println("Pablo");
         System.out.println("Nací en León en 2002");
-        System.oiut.println("Actualmente vivo en Eras de Renueva en León y tengo instalado windows 10");
+        System.out.println("Actualmente vivo en Eras de Renueva en León y tengo instalado windows 10");
+        System.out.println("Pablo Gutiérrez Ramos");
         
     }
 }
+-
